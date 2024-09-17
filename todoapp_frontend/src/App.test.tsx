a7da91b9556fee3948171b4357c7748dc69f9365
Miguel Castillo <miguel.leyva@encora.com>
2024-09-17 15:53:29 -0700

    further tests

--- todoapp_frontend/src/App.test.tsx
+++ todoapp_frontend/src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ToDoList from './components/ToDoList';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   const {container} = render(<App />);
   expect(container.firstChild);
+});
+
+test('renders todo list', () => {
+  const {container} = render(<App />);
+  expect(container.className == "ToDoList");
 });
